1be9e33b220929532b07f3d887b8e61e8b24ae17
ys36-36 <sidhouiza36@proton.me>
2024-09-17 12:41:55 +0100

    added ui

--- index.js
+++ index.js
@@ -1,73 +1,80 @@
-let i=1;
+
 let HumanScore = 0, ComputerScore = 0, numDraws = 0;
 
-while (i<6) {
     function getComputerChoice() {
         let x = Math.floor(Math.random()*3)+1;        
         return x;
     };
-    function getHumanChoice() {
-        let choice = prompt("Rock? Paper? Scissors?");
-        choice = choice.toUpperCase();
-        switch (choice) {
-            case "ROCK": return 1;
-            case "PAPER": return 2;
-            case "SCISSORS": return 3;
-        }
-    }
 
+    function roundIssue(compRes,humRes,draw) {
+        let container = document.querySelector('#resultTab');
+        container.textContent = "";
+        let resultTab = document.createElement("p");
+        container.appendChild(resultTab);
+        resultTab.textContent = `Computer Score: ${compRes} 
+                                Human Score: ${humRes}
+                                Draws: ${draw}`;  
+    }
+    
+    
     function playRound(human,computer) {
         switch (human) {
             case 1: {
                 if (computer===1) {
                     numDraws+=1;
-                    console.log("That's a draw");
+                    ;
                 } else if (computer===2) {
                     ComputerScore+=1;
-                    console.log("The computer wins the round!");
                 } else {
                     HumanScore+=1;
-                    console.log("The human wins the round!");
                 }
+                roundIssue(ComputerScore,HumanScore,numDraws)
             }
             break;
             case 2: {
                 if (computer===2) {
                     numDraws+=1;
-                    console.log("That's a draw");
                 } else if (computer===3) {
                     ComputerScore+=1;
-                    console.log("The computer wins the round!");
                 } else {
                     HumanScore+=1;
-                    console.log("The human wins the round!");
                 }
+                roundIssue(ComputerScore,HumanScore,numDraws)
             } 
             break;
             case 3: {
                 if (computer===3) {
                     numDraws+=1;
-                    console.log("That's a draw");
                 } else if (computer===1) {
                     ComputerScore+=1;
-                    console.log("The computer wins the round!");
                 } else {
                     HumanScore+=1;
-                    console.log("The human wins the round!");
                 }
+                roundIssue(ComputerScore,HumanScore,numDraws)
             }
             break;
         }
+        if (ComputerScore == 5){
+            let container = document.querySelector('#resultTab');
+            container.textContent = "";
+            let resultTab = document.createElement("p");
+            container.appendChild(resultTab);
+            resultTab.textContent = "Computer wins";
+            HumanScore = 0, ComputerScore = 0, numDraws = 0;
+        } else if (HumanScore == 5) {
+            let container = document.querySelector('#resultTab');
+            container.textContent = "";
+            let resultTab = document.createElement("p");
+            container.appendChild(resultTab);
+            resultTab.textContent = "Human wins";
+            HumanScore = 0, ComputerScore = 0, numDraws = 0;
+        }
     }
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-
-    i+=1;
-}
-
-console.log(`Human wins : ${HumanScore}
-Computer wins : ${ComputerScore}
-Number of draws : ${numDraws}`)
+    
+    const Rock = document.querySelector("#Rock");
+    Rock.addEventListener("click",() => playRound(1,getComputerChoice()));
+    const Paper = document.querySelector("#Paper");
+    Paper.addEventListener("click",() => playRound(2,getComputerChoice()));
+    const Scissors = document.querySelector("#Scissors");
+    Scissors.addEventListener("click",() => playRound(3,getComputerChoice()));
